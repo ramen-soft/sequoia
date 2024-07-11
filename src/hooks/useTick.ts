12b86a7d91b12baecaebf496e85ae6dc424491de
ramen-soft <ramen.devel@gmail.com>
2024-07-11 17:51:45 +0200

    Provide a hook `useTick` that listen to events triggered by TimerContext

---
+++ src/hooks/useTick.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from "react"
+
+export const useTick = (onTick : ()=>void) => {
+    const callbackRef = useRef(onTick)
+
+    useEffect(()=>{
+        callbackRef.current = onTick
+    }, [onTick])
+
+    useEffect(()=>{
+        const handleEvent = (event : any) => {
+            if(event.detail.type == 'semicorchea'){
+                callbackRef.current();
+            }
+        };
+        window.addEventListener('timerTick', handleEvent);
+
+        return ()=>{
+            window.removeEventListener('timerTick', handleEvent);
+        }
+    })
+}
