67118edf0ae9ee38d48b57d3565e4b6aeffaf406
ramen-soft <ramen.devel@gmail.com>
2024-07-11 17:46:40 +0200

    Expose types describing parts of a project

---
+++ src/models/models.ts
@@ -0,0 +1,26 @@
+export interface ChannelDefinition{
+    name: string;
+    instrument: string;
+    type: 'roll'|'pad';
+    notes: any[];
+    checkpoints ?: any[];
+}
+
+export interface PatternDefinition{
+    name: string;
+    steps: number;
+    channels: ChannelDefinition[]
+}
+
+export interface TrackDefinition{
+
+}
+
+export interface Project{
+    title: string;
+    bpm: number;
+    instruments: string[];
+    patterns: PatternDefinition[];
+    tracks: TrackDefinition[];
+    editingPattern ?: PatternDefinition
+};
