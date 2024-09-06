58e1f384d959dd7b34020f41c08b8f0b968a7d30
Raúl Menghini <raul@bn-tic.es>
2024-09-06 13:16:38 +0200

    Added a temporary button to load a preset pattern

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { SeqContext } from './context/SeqContext';
 import { PatternEditor } from './components/playlist/PatternEditor';
 import { PianoRoll } from './components/pianoroll/PianoRoll';
 import { useProjectStore } from './states/ProjectState';
+import { PatternDefinition } from './models/models';
 
 function App() {
 
@@ -49,6 +50,12 @@ function App() {
       <PianoRoll pattern={project.patterns[0]} channel={project.patterns[0].channels[0]}>
                 
       </PianoRoll>
+
+      <button onClick={()=>{
+        const pattern : PatternDefinition = {"name":"Pattern 01","steps":16,"channels":[{"instrument":"sequoia/piano","name":"Channel 01","checkpoints":[{"note":261.626,"time":{"start":0,"end":50,"duration":156.25},"step":0},{"note":261.626,"time":{"start":156,"end":206,"duration":156.25},"step":1},{"note":391.995,"time":{"start":312,"end":362,"duration":156.25},"step":2},{"note":261.626,"time":{"start":468,"end":518,"duration":156.25},"step":3},{"note":415.305,"time":{"start":625,"end":675,"duration":156.25},"step":4},{"note":261.626,"time":{"start":781,"end":831,"duration":156.25},"step":5},{"note":391.995,"time":{"start":937,"end":987,"duration":156.25},"step":6},{"note":261.626,"time":{"start":1093,"end":1143,"duration":156.25},"step":7},{"note":349.228,"time":{"start":1250,"end":1300,"duration":156.25},"step":8},{"note":311.127,"time":{"start":1406,"end":1456,"duration":156.25},"step":9},{"note":293.665,"time":{"start":1562,"end":1612,"duration":156.25},"step":10},{"note":311.127,"time":{"start":1718,"end":1768,"duration":156.25},"step":11},{"note":349.228,"time":{"start":1875,"end":1925,"duration":156.25},"step":12},{"note":311.127,"time":{"start":2031,"end":2081,"duration":156.25},"step":13},{"note":293.665,"time":{"start":2187,"end":2237,"duration":156.25},"step":14},{"note":233.082,"time":{"start":2343,"end":2393,"duration":156.25},"step":15}],"notes":[],"type":"pad"},{"instrument":"sequoia/kick","name":"Channel 02","notes":[],"checkpoints":[{"note":261.626,"time":{"start":0,"end":50,"duration":156.25},"step":0},{"note":261.626,"time":{"start":625,"end":675,"duration":156.25},"step":4},{"note":261.626,"time":{"start":1250,"end":1300,"duration":156.25},"step":8},{"note":261.626,"time":{"start":1875,"end":1925,"duration":156.25},"step":12}],"type":"pad"},{"instrument":"sequoia/snare","name":"Channel 03","notes":[],"checkpoints":[{"note":261.626,"time":{"start":625,"end":675,"duration":156.25},"step":4},{"note":261.626,"time":{"start":1875,"end":1925,"duration":156.25},"step":12}],"type":"pad"},{"instrument":"sequoia/hithat","name":"Channel 04","notes":[],"checkpoints":[{"note":261.626,"time":{"start":0,"end":50,"duration":156.25},"step":0},{"note":261.626,"time":{"start":312,"end":362,"duration":156.25},"step":2},{"note":261.626,"time":{"start":625,"end":675,"duration":156.25},"step":4},{"note":261.626,"time":{"start":937,"end":987,"duration":156.25},"step":6},{"note":261.626,"time":{"start":1250,"end":1300,"duration":156.25},"step":8},{"note":261.626,"time":{"start":1562,"end":1612,"duration":156.25},"step":10},{"note":261.626,"time":{"start":1875,"end":1925,"duration":156.25},"step":12},{"note":261.626,"time":{"start":2187,"end":2237,"duration":156.25},"step":14}],"type":"pad"}]};
+        console.log(pattern);
+        project.setPattern(0, pattern);
+      }}>Load song</button>
     </>
   );
 }
